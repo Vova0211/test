1f7e18906901cb7bf0d1c9f1c57549c97a1c71cf
Vova0211 <chernousov_07@bk.ru>
2024-11-06 09:33:12 +0300

    test JS code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('lf'); 
+const printQuestion = (word = '1') => {
+    console.log(`${word}?`)
+}
+
+const printQuestion2 = (text) => {
+    console.log(text + '?')
+    /**/
+}
+  
+const getHiddenCard = (num, amount = 4) => {
+    let result = `${'*'.repeat(amount)}${num.slice(16 - 4)}`;
+    return(result);
+}
+
+const capitalize = (text) => {
+    
+    return(result);
+}
