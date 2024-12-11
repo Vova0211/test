774b4ffecf826e88b41f4bcdd54db8e4bfe05489
Vova0211 <chernousov_07@bk.ru>
2024-12-11 11:16:40 +0300

    complete gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { parallel } = require("gulp");
+const { parallel, src, dest } = require("gulp");
 
 const myFirstGulpTask = (done) => {
   console.log("My First Gulp Task Completed");
@@ -8,6 +8,11 @@ const mySecondGulpTask = (done) => {
   console.log("My Second Gulp Task Completed");
   done();
 }
+const copyCSS = () => {
+  return src(['work_for_04_12/**/*.css', '!work_for_04_12/da/**'])
+    .pipe(dest('assets/'));
+}
 exports.default = parallel(myFirstGulpTask, mySecondGulpTask);
 exports.myFirstGulpTask = myFirstGulpTask;
 exports.mySecondGulpTask = mySecondGulpTask;
+exports.copyCSS = copyCSS;
