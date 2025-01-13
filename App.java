cbe8bfb9847a42e5c9b410ed25cc535c707b8756
Vova0211 <chernousov_07@bk.ru>
2025-01-13 17:42:06 +0300

    js

--- App.java
+++ App.java
@@ -20,23 +20,23 @@ public class App {
         // BEGIN (write your solution here)
         String result = "";
         for(int i = 0; i < str.length(); i++) {
-        if (str.length() % 2 == 0) {
-          if(i % 2 == 0) {
-              result += str.charAt(i+1);
-          } else {
-              result += str.charAt(i-1);
-          }
-        } else {
-            if (i == (str.length() - 1)) {
-                result += str.charAt(i);
-            } else if(i % 2 == 0){
-                result += str.charAt(i+1);
+            if (str.length() % 2 == 0) {
+                if(i % 2 == 0) {
+                    result += str.charAt(i+1);
+                } else {
+                    result += str.charAt(i-1);
+                }
             } else {
-                result += str.charAt(i-1);
-            }
-        } 
-    }
-    return result;
+                if (i == (str.length() - 1)) {
+                    result += str.charAt(i);
+                } else if(i % 2 == 0){
+                    result += str.charAt(i+1);
+                } else {
+                    result += str.charAt(i-1);
+                }
+            } 
+        }
+        return result;
         // END
     }
 }
---
+++ script.js
@@ -0,0 +1,17 @@
+const p1 = document.getElementById('name');
+const name_1 = p1.value;
+
+// const item = document.querySelector('#item');
+// let text = item.textContent;
+// console.log(p1.value);
+// console.log(p1)
+
+console.log(sum(40, 12))
+
+const sum = (num1, num2) => {
+  return num1 + num2;
+}
+function sum1(num1, num2) {
+  return num1 + num2;
+}
+
