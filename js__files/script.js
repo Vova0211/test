58af614342733043a8f2a6d17ab40480ceb5a0cb
Vova0211 <chernousov_07@bk.ru>
2025-01-26 12:31:29 +0300

    update scripts

--- js__files/script.js
+++ js__files/script.js
@@ -1,17 +1,11 @@
-const p1 = document.getElementById('name');
-const name_1 = p1.value;
+// const p1 = document.getElementById('name');
+// const name_1 = p1.value;
 
 // const item = document.querySelector('#item');
 // let text = item.textContent;
 // console.log(p1.value);
 // console.log(p1)
-
-console.log(sum(40, 12))
-
-const sum = (num1, num2) => {
-  return num1 + num2;
+function getInfo() {
+  const name = document.getElementById("name").value;
+  console.log(name);
 }
-function sum1(num1, num2) {
-  return num1 + num2;
-}
-
